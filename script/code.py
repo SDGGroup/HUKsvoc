fix feature calculation, add output preparation

script/code.py
@@ -1,17 +1,14 @@
 
-import importlib
+# import importlib
 # importlib.reload()
 from svoc.settings import get_settings
 from svoc.utils import read_data
 from svoc.datapreparation import prepare_data, make_upper_str, rename_and_select_cols
 from svoc.automatic.match import get_automatic_matches
 from svoc.supervised.match import predict_supervised
-from svoc.rl import get_matches
+from svoc.rl import get_matches, prepare_output
 import pandas as pd
-import numpy as np
-import openpyxl
-
-from svoc import constants as cons
+from svoc.constants import DISTANCES, FILTERS_AUTO
 
 
 # settings = get_settings()
@@ -19,8 +16,9 @@ settings = get_settings("./config/dev2.yaml")
 
 df_input, df_benchmark = read_data(settings)
 
+
 # ## Modifico Location SAP
-# df_loc = pd.read_csv('./data/HUK_sap_location.csv', sep=',', dtype=str)
+# df_loc = pd.read_csv('./data/HUK_sap_location.csv', sep=',', dtype=str)*
 # cols_to_join = ["AddressLine1", "AddressLine2", "AddressLine3", "AddressLine4"]
 # df_loc[settings.BENCHMARK_COLUMNS.ADDRESS] = df_loc[cols_to_join].apply(
 #     lambda x: ', '.join(x.dropna().astype(str)), 
@@ -42,15 +40,6 @@ df_input, df_benchmark = read_data(settings)
 # df_input_clean = prepare_data(
 #     df=df_input, dict_cols=settings.INPUT_COLUMNS_DICT,
 #     parse_address=True, get_town=False, rm_address_noise=True)
-
-## BOWIMI Benchmark - CGA Input (dev2.yaml)
-df_benchmark_clean = prepare_data(
-    df=df_benchmark, dict_cols=settings.BENCHMARK_COLUMNS_DICT,
-    parse_address=True, get_town=False, rm_address_noise=True)
-df_input_clean = prepare_data(
-    df=df_input, dict_cols=settings.INPUT_COLUMNS_DICT,
-    parse_address=False, get_town=False, rm_address_noise=True)
-
 # df_inner = (df_input_clean
 #             .merge(
 #                 make_upper_str(df_input[[settings.INPUT_COLUMNS.ID, "SapCode"]]).replace(['NAN', 'NONE'], np.nan), 
@@ -62,6 +51,16 @@ df_input_clean = prepare_data(
 #                     left_on=settings.BENCHMARK_COLUMNS.ID, right_on='ID', 
 #                     how='inner', suffixes=('_input', '_benchmark'))
 #             )
+
+## BOWIMI Benchmark - CGA Input (dev2.yaml)
+df_benchmark_clean = prepare_data(
+    df=df_benchmark, dict_cols=settings.BENCHMARK_COLUMNS_DICT,
+    parse_address=True, get_town=False, rm_address_noise=True)
+df_input_clean = prepare_data(
+    df=df_input, dict_cols=settings.INPUT_COLUMNS_DICT,
+    parse_address=False, get_town=False, rm_address_noise=True)
+
+
 #----------------------------------------------------------
 
 ## Auto + Superv Matching wrapper
@@ -69,10 +68,19 @@ all_matches, features, remaining_features = get_matches(
     df_input=df_input_clean, 
     df_benchmark=df_benchmark_clean, 
     block_col=settings.BLOCK_COL, 
-    distances_dict=cons.DISTANCES, 
-    filters_dict=cons.FILTERS_AUTO,
+    distances_l=DISTANCES, 
+    filters_dict=FILTERS_AUTO,
     n_groups=15, n_matches=settings.N_MATCHES, verbose=False
     )
+
+output = prepare_output(
+    matches=all_matches,
+    distances_l=DISTANCES,
+    filters_dict=FILTERS_AUTO
+)
+
+output[output["ID_filter"]==45]
+output[output["ID_filter"].isna()]
 
 #----------------------------------------------------------
 ## Automatic Matching
@@ -80,8 +88,8 @@ all_matches_auto, features, remaining_features = get_automatic_matches(
     df_input=df_input_clean, 
     df_benchmark=df_benchmark_clean, 
     block_col=settings.BLOCK_COL, 
-    distances_dict=cons.DISTANCES, 
-    filters_dict=cons.FILTERS_AUTO,
+    distances_dict=DISTANCES, 
+    filters_dict=FILTERS_AUTO,
     n_groups=15, n_matches=settings.N_MATCHES, verbose=False)
 
 all_matches_auto = (
@@ -92,7 +100,7 @@ all_matches_auto = (
 
 ## Check filters
 all_matches_auto['ID_filter'].value_counts().sort_index()
-[cons.FILTERS_AUTO[i-1] for i in all_matches_auto['ID_filter'].drop_duplicates().sort_values().tolist()]
+[FILTERS_AUTO[i-1] for i in all_matches_auto['ID_filter'].drop_duplicates().sort_values().tolist()]
 
 all_matches_auto
 

svoc/automatic/features.py
@@ -1,44 +1,60 @@
 import pandas as pd
 import numpy as np
-import recordlinkage as rl
+from recordlinkage import Compare, Index
+from svoc.automatic.enums import DistanceMethod, Distance
 
-def initialize_compare_cl(l_compare, n_jobs_param):
-    compare_cl = rl.Compare(n_jobs=n_jobs_param)
+def initialize_compare_cl(
+        l_compare: list[Distance], 
+        n_jobs_param: int = -1
+    ) -> Compare:
+    compare_cl = Compare(n_jobs=n_jobs_param)
     for element in l_compare:
-        if 'method' in element:
-            compare_cl.string(element['col_name'], element['col_name'], label=element['label'],
-                              method=element['method'], missing_value=0)
+        if (
+            element.method is not None 
+            and element.method!=DistanceMethod.EXACT
+            and element.method!=DistanceMethod.SUBSTRING
+            and element.method!=DistanceMethod.WORDSMATCH
+        ):
+            compare_cl.string(element.col_name, element.col_name, label=element.label,
+                              method=element.method, missing_value=0)
+        elif element.method == DistanceMethod.EXACT:
+            compare_cl.exact(element.col_name, element.col_name, label=element.label, missing_value=0)
         else:
-            compare_cl.exact(element['col_name'], element['col_name'], label=element['label'], missing_value=0)
+            pass  # Substring and Wordsmatch are handled separately
+            
     return compare_cl
 
-def rl_compare_block(df_1, df_2, compare_cl, block_variable):
-    indexer = rl.Index()
-    indexer.block(block_variable)
+def rl_compare_block(
+        df_1: pd.DataFrame, 
+        df_2: pd.DataFrame, 
+        compare_cl: Compare, 
+        block_variable: str|None = None
+    ) -> pd.DataFrame:
+    indexer = Index()
+    if block_variable is not None:
+        indexer.block(block_variable)
     candidate_links = indexer.index(df_1, df_2)
     features = compare_cl.compute(candidate_links, df_1, df_2)
     features = features.fillna(0.0)
     return features
 
-def arrange_features(features, df_1, df_2, index_x, index_y):
-    original_columns_list = features.columns.tolist()
+def vec_substring_matching(
+        column_x: pd.Series, 
+        column_y: pd.Series
+    ) -> pd.Series:
+    
+    out = 1 * np.array([
+        (str(x) in str(y)) or (str(y) in str(x))
+        if pd.notna(x) and pd.notna(y) else 0
+        for x, y in zip(column_x, column_y)
+    ])
+    return pd.Series(out, index=column_x.index, dtype="int64")
 
-    features.reset_index(level=[index_x, index_y], inplace=True)
-    features = features.merge(df_1, how='left', left_on=index_x, right_on="ID")
-    features = features.merge(df_2, how='left', left_on=index_y, right_on="ID")
 
-    features['outlet_name_in'] = 1 * np.array(
-        [(str(x) in str(y)) or (str(y) in str(x)) if pd.notna(x) and pd.notna(y) else 0 for x, y in
-         zip(features['OUTLET_NAME_x'], features['OUTLET_NAME_y'])])
-    features['outlet_name_clean_in'] = 1 * np.array(
-        [(str(x) in str(y)) or (str(y) in str(x)) if pd.notna(x) and pd.notna(y) else 0 for x, y in
-         zip(features['OUTLET_NAME_CLEAN_x'], features['OUTLET_NAME_CLEAN_y'])])
-    features['address_in'] = 1 * np.array(
-        [(str(x) in str(y)) or (str(y) in str(x)) if pd.notna(x) and pd.notna(y) else 0 for x, y in
-         zip(features['ADDRESS_x'], features['ADDRESS_y'])])
-    features['address_clean_in'] = 1 * np.array(
-        [(str(x) in str(y)) or (str(y) in str(x)) if pd.notna(x) and pd.notna(y) else 0 for x, y in
-         zip(features['ADDRESS_CLEAN_x'], features['ADDRESS_CLEAN_y'])])
+def vec_word_subset(
+    column_x: pd.Series, 
+    column_y: pd.Series
+    ) -> pd.Series:
 
     def contains_all_words(x, y):
         if pd.isna(x) or pd.isna(y):
@@ -47,32 +63,56 @@ def arrange_features(features, df_1, df_2, index_x, index_y):
         w2 = set(str(y).split())
         return int(w1.issubset(w2) or w2.issubset(w1))
 
-    features['outlet_name_in2'] = [
-        contains_all_words(x, y)
-        for x, y in zip(features['OUTLET_NAME_x'], features['OUTLET_NAME_y'])
-    ]
-    features['outlet_name_clean_in2'] = [
-        contains_all_words(x, y)
-        for x, y in zip(features['OUTLET_NAME_CLEAN_x'], features['OUTLET_NAME_CLEAN_y'])
-    ]
-    features['address_in2'] = [
-        contains_all_words(x, y)
-        for x, y in zip(features['ADDRESS_x'], features['ADDRESS_y'])
-    ]
-    features['address_clean_in2'] = [
-        contains_all_words(x, y)
-        for x, y in zip(features['ADDRESS_CLEAN_x'], features['ADDRESS_CLEAN_y'])
-    ]
+    return pd.Series(
+        (contains_all_words(x, y) for x, y in zip(column_x, column_y)),
+        index=column_x.index,
+        dtype="int64"
+    )
 
+
+def manual_features(
+        l_compare: list[Distance], 
+        features: pd.DataFrame, 
+        x: pd.DataFrame, 
+        y: pd.DataFrame,
+        index_x: str, 
+        index_y: str,
+    ) -> pd.DataFrame:
+
+    original_cols = features.columns.tolist()
+
+    # Merge
+    features = features.reset_index(level=[index_x, index_y])
+    features = features.merge(x.add_suffix('_x'), how='left', left_on=index_x, right_on='ID')
+    features = features.merge(y.add_suffix('_y'), how='left', left_on=index_y, right_on='ID')
     
-    final_columns_list = [index_x, index_y] + original_columns_list + \
-        ['outlet_name_in', 'outlet_name_clean_in', 'address_in', 'address_clean_in'] + \
-        ['outlet_name_in2', 'outlet_name_clean_in2', 'address_in2', 'address_clean_in2']
+    new_cols = []
+    for element in l_compare:
+        if element.method == DistanceMethod.SUBSTRING:
+            features[element.label] = vec_substring_matching(
+                                   column_x=features[f'{element.col_name}_x'],
+                                   column_y=features[f'{element.col_name}_y']
+                                   )
+            new_cols.append(element.label)
+        elif element.method == DistanceMethod.WORDSMATCH:
+            features[element.label] = vec_word_subset(
+                                   column_x=features[f'{element.col_name}_x'],
+                                   column_y=features[f'{element.col_name}_y']
+                                   )
+            new_cols.append(element.label)
+        else:
+            pass
+    
+    return features[[index_x, index_y] + original_cols + new_cols]
 
-    return features[final_columns_list]
 
-def get_features(distances_dict, df_x, df_y, block_col):
-    compare_cl = initialize_compare_cl(distances_dict, n_jobs_param=-1)
+def get_features(
+        distances: list[Distance], 
+        df_x: pd.DataFrame, 
+        df_y: pd.DataFrame, 
+        block_col: str|None = None
+    ) -> pd.DataFrame:
+    compare_cl = initialize_compare_cl(distances, n_jobs_param=-1)
     features = rl_compare_block(df_x, df_y, compare_cl, block_col)
-    features = arrange_features(features, df_1=df_x, df_2=df_y, index_x="ID_1", index_y="ID_2")
+    features = manual_features(distances, features, df_x, df_y, index_x="ID_1", index_y="ID_2")
     return features

svoc/datapreparation.py
@@ -107,10 +107,28 @@ def parse_address_components(df, get_town=True):
     
     return out.drop(columns=['processAdd'])
 
-def remove_accents_and_regex(df, re_pattern, l_id_cols, l_cols_not_to_apply=[]):
+def remove_accents_and_regex(
+        df: pd.DataFrame, 
+        re_pattern: str, 
+        l_id_cols: list, 
+        l_cols_not_to_apply: list | None = None
+    ) -> pd.DataFrame:
+    
+    if l_cols_not_to_apply is None:
+        l_cols_not_to_apply = []
+
     df_out = df.replace(['NAN', 'nan', 'NONE'], '')
-    df_out[df_out.columns.difference(l_id_cols+l_cols_not_to_apply)] = df_out[df_out.columns.difference(l_id_cols+l_cols_not_to_apply)].applymap(lambda x: unidecode(x) if pd.notna(x) else x)
-    df_out[df_out.columns.difference(l_id_cols+l_cols_not_to_apply)] = df_out[df_out.columns.difference(l_id_cols+l_cols_not_to_apply)].apply(lambda col: col.str.replace(re_pattern, '', regex=True))
+
+    cols_to_clean = df_out.columns.difference(l_id_cols + l_cols_not_to_apply)
+
+    df_out[cols_to_clean] = df_out[cols_to_clean].map(
+        lambda x: unidecode(x) if pd.notna(x) else x
+    )
+
+    df_out[cols_to_clean] = df_out[cols_to_clean].apply(
+        lambda col: col.str.replace(re_pattern, '', regex=True)
+    )
+
     df_out = df_out.replace('', np.nan)
     return df_out
 